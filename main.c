f0089883209e304754af07f33c783290d5e54f86
choudribasavaraj28-wq <choudribasavaraj28@gmail.com>
2026-06-06 15:11:44 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+#define ROWS 20
+#define COLS 40
+char canvas[ROWS][COLS];
+void clearcanvas()
+{
+  int i,j;
+  for(int i=0;i<n;i++)
+    {
+      canvas[i][j]='_';
+    }
+}
+void putPixel(int x,int y,char ch)
+{
+  if(x>=0 && x<COLS && y>=0 && y<ROWS)
+  {
+  canvas[y][x]=ch;
+  }
+}  
+void display()
+{
+  int i,j;
+  for(i=o;i<ROWS;i++)
+    {
+      for(j=0;j<COLS;j++)
+        {
+          printf("%c",canvas[i][j]);
+        }
+      printf("\n");
+    }
+}
+int main()
+{
+  clearcanvas();
+  putPixel(5,5,'*');
+  putPixel(10,10,'*');
+  displayCanvas();
+  return 0;
+}
   
