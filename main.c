259401958ebc9505f38d23c1d72a8508beebb473
choudribasavaraj28-wq <choudribasavaraj28@gmail.com>
2026-06-08 18:54:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-#define ROWS 20
-#define COLS 40
+#include<stdlib.h>
+#define ROWS 35
+#define COLS 80
 char canvas[ROWS][COLS];
-void clearcanvas()
+void clearCanvas()
 {
   int i,j;
   for(i=0;i<ROWS;i++)
     {
       for(j=0;j<COLS;j++)
       {
-      canvas[i][j]='_';
+      canvas[i][j]=' ';
       }
     }
 }
@@ -32,7 +33,7 @@ void displayCanvas()
       printf("\n");
     }
 }
-void drawLineCanvas(int x1,int y1
+void drawLineCanvas(int x1,int y1,
                     int x2,int y2,
                     char ch)
 {
@@ -50,7 +51,7 @@ void drawLineCanvas(int x1,int y1
     int e2=2*err;
     if(e2>-dy){
       err-=dy;
-      x1+-sx;
+      x1+=sx;
     }
     if(e2<dx){
       err+=dx;
@@ -59,10 +60,30 @@ void drawLineCanvas(int x1,int y1
   }
     
 }
+void drawRectangleCanvas(int x1,int y1,
+                        int x2,int y2,
+                        char ch)
+{
+  drawLineCanvas(x1,y1,x2,y1,ch);
+  drawLineCanvas(x2,y1,x2,y2,ch);
+  drawLineCanvas(x2,y2,x1,y2,ch);
+  drawLineCanvas(x1,y2,x1,y1,ch);
+}
+void drawTriangleCanvas(int x1,int y1,
+                        int x2, int y2,
+                        int x3,int y3,
+                        char ch)
+{
+  drawLineCanvas(x1,y1,x2,y2,ch);
+  drawLineCanvas(x2,y2,x3,y3,ch);
+  drawLineCanvas(x3,y3,x1,y1,ch);
+}
 int main()
 {
-  clearcanvas();
-  drawLineCanvas(2,2,25,10,'*')
+  clearCanvas();
+  drawLineCanvas(2,2,15,10,'*');
+  drawRectangleCanvas(5,12,20,18,'*');
+  drawTriangleCanvas(25,5,35,15,15,15,'*');
   displayCanvas();
   return 0;
 }
